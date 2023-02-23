aa0ec313181f8beb7cd58c897f9b214b61e3e0d4
JBBianchi <jb.bianchi@neuroglia.io>
2023-02-23 14:28:51 +0100

    feat(Dashboard): added read options form (replaces partition filter)

---
+++ src/dashboard/ui/Components/ReadOptionsForm/State.cs
@@ -0,0 +1,36 @@
+﻿namespace CloudStreams.Dashboard.Components.ReadOptionsFormStateManagement;
+
+/// <summary>
+/// Represents the state of the form used to manipulate <see cref="CloudEventStreamReadOptions"/>
+/// </summary>
+public record ReadOptionsFormState
+{
+    /// <summary>
+    /// Gets/sets a reference to the cloud event partition's type to read, if any
+    /// </summary>
+    public CloudEventPartitionType? PartitionType { get; set; } = null;
+    /// <summary>
+    /// Gets/sets a reference to the cloud event partition's id to read, if any
+    /// </summary>
+    public string? PartitionId { get; set; } = null;
+
+    /// <summary>
+    /// Gets/sets the direction in which to read the stream of cloud events
+    /// </summary>
+    public StreamReadDirection Direction { get; set; } = StreamReadDirection.Backwards;
+
+    /// <summary>
+    /// Gets/sets the offset starting from which to read the stream
+    /// </summary>
+    public long? Offset { get; set; } = null;
+
+    /// <summary>
+    /// Gets/sets the amount of events to read from the stream
+    /// </summary>
+    public ulong? Length { get; set; } = null;
+
+    /// <summary>
+    /// Gets the <see cref="List{T}"/> of suggested <see cref="CloudEventPartitionRef"/>s
+    /// </summary>
+    public List<CloudEventPartitionMetadata>? Partitions { get; set; } = new();
+}
