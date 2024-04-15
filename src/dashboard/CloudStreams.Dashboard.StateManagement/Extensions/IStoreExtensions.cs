a3ef18b1f831a7c20c73ba3d7c8b324323972686
Charles d'Avernas <charles.davernas@neuroglia.io>
2024-04-15 10:36:38 +0200

    fix(Solution): Fixed all csproj files to include package info fix(Solution): Prefixed all files with CloudStreams copyright notice

--- src/dashboard/CloudStreams.Dashboard.StateManagement/Extensions/IStoreExtensions.cs
+++ src/dashboard/CloudStreams.Dashboard.StateManagement/Extensions/IStoreExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2023-Present The Cloud Streams Authors
+﻿// Copyright © 2024-Present The Cloud Streams Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License"),
 // you may not use this file except in compliance with the License.
